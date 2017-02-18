91297de386bed05fea93c025497d8230452fbba4
Andrew Dawidek <dawidekandy@hotmail.com>
2017-02-18 10:45:52 -0500

    More replacing of tabs with spaces

--- entitymanager.lua
+++ entitymanager.lua
@@ -3,35 +3,35 @@ EntityManager = Object:extend()
 require "sheep"
 
 function EntityManager:new()
-	self.entities = {}
+    self.entities = {}
 end
 
 function EntityManager:draw()
-	for i,entity in ipairs(self.entities) do
-		entity:draw()
-	end
+    for i,entity in ipairs(self.entities) do
+    entity:draw()
+    end
 end
 
 function EntityManager:update(dt)
-	for i,entity in ipairs(self.entities) do
-		entity:update(dt)
-	end
+    for i,entity in ipairs(self.entities) do
+        entity:update(dt)
+    end
 end
 
 function EntityManager:CreateBoxEntity(x, y, physics)
-	local entity = Entity(Assets.Graphics.Sprites.Box, x, y)
-	if physics then
-		entity:RegisterPhysics(64, 64, "dynamic")
-	end
-	return self:AddEntity(entity)
+    local entity = Entity(Assets.Graphics.Sprites.Box, x, y)
+    if physics then
+        entity:RegisterPhysics(64, 64, "dynamic")
+    end
+    return self:AddEntity(entity)
 end
 
 function EntityManager:CreateSheep(x, y)
-	local sheep = Sheep(x, y)
-	return self:AddEntity(sheep)
+    local sheep = Sheep(x, y)
+    return self:AddEntity(sheep)
 end
 
 function EntityManager:AddEntity(entity)
-	table.insert(self.entities, entity)
-	return entity
+    table.insert(self.entities, entity)
+    return entity
 end
--- level.lua
+++ level.lua
@@ -5,20 +5,20 @@ require "entitymanager"
 local mEntityManager = nil
 
 function Level:new(stageNum)
-	mEntityManager = EntityManager()
-	self.stage = stageNum
-	self:load()
+    mEntityManager = EntityManager()
+    self.stage = stageNum
+    self:load()
 end
 
 function Level:draw()
-	mEntityManager:draw()
+    mEntityManager:draw()
 end
 
 function Level:update(dt)
-	mEntityManager:update(dt)
+    mEntityManager:update(dt)
 end
 
 function Level:load()
-	mEntityManager:CreateSheep(200, 100)
-	mEntityManager:CreateSheep(400, 100)
+    mEntityManager:CreateSheep(200, 100)
+    mEntityManager:CreateSheep(400, 100)
 end
